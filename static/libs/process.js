66cbc59151aec386a09b21d2b3cefcf016f78e3d
GsLogimaker <gslogimaker@proton.me>
2025-11-10 15:54:05 -0600

    Update process.js

--- static/libs/process.js
+++ static/libs/process.js
@@ -6,10 +6,10 @@ window.onload = function() {
   	
 	let prev = addNav.getAttribute('prev');
 	let next = addNav.getAttribute('next');
-	let links ="<a href= '/Revivavl/${prev}'>Prev</a>"
-		+ "<a href= '/Revivavl/1/1'>First</a>"
-		+ "<a href= '/Revivavl/${next}'>Next</a>"
-	let nav = "<center> ${links} </center>";
+	let links = `<a href= '/Revivavl/${prev}'>Prev</a>`
+		+ `<a href= '/Revivavl/1/1'>First</a>`
+		+ `<a href= '/Revivavl/${next}'>Next</a>`
+	let nav = `<center> ${links} </center>`;
 	
 	header.innerHTML += nav;
 	footer.innerHTML = nav + footer.innerHTML;
