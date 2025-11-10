38c36da7858ecc6c92b2a83b1a846f2342014803
GsLogimaker <gslogimaker@proton.me>
2025-11-10 16:07:12 -0600

    Update process.js

--- static/libs/process.js
+++ static/libs/process.js
@@ -4,16 +4,16 @@ window.onload = function() {
 	let header = document.querySelector('header');
 	let footer = document.querySelector('footer');
   	
-	let prev = addNav.getAttribute('prev');
-	let next = addNav.getAttribute('next');
-	let links = `<a href= '/Revivavl/${prev}'>Prev</a>`
-		+ `<a href= '/Revivavl/1/1'>First</a>`
-		+ `<a href= '/Revivavl/${next}'>Next</a>`
-	let nav = `<center> ${links} </center>`;
+	let prev = `/Revival/${addNav.getAttribute('prev')}`.replace("/Revival//", "/Revival/");
+	let next = `/Revival/${addNav.getAttribute('next')}`.replace("/Revival//", "/Revival/");
+	let links = `<a href='${prev}'>Prev</a>`
+		+ `<a href='/Revivavl/1/1'>First</a>`
+		+ `<a href='${next}'>Next</a>`
+	let nav = `<center>${links}</center>`;
 	
-	header.innerHTML += nav;
-	if (footer) {
-		footer.innerHTML = nav + footer.innerHTML;
-	}
+	// header.innerHTML += nav;
+	// if (footer) {
+	// 	footer.innerHTML = nav + footer.innerHTML;
+	// }
 
 };
